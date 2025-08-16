1a7d3aa0c8e2913913166ddb67a326ddd903503c
Santosh Kanojiya <kanojiyas780@gmail.com>
2025-08-16 05:21:50 +0000

    Ok show me query so I can run in my sql

---
+++ supabase/migrations/20240628120005_create_purge_notifications_function.sql
@@ -0,0 +1,7 @@
+create or replace function public.purge_old_notifications()
+returns void
+language sql
+as $$
+  delete from public.notifications
+  where created_at < now() - interval '24 hours';
+$$;
